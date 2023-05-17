6fc30357753fd4183311f8002342630dd196bbe9
babotavo419 <one.faith419@gmail.com>
2023-05-17 05:48:58 -0400

    so far so good

--- src/index.js
+++ src/index.js
@@ -1,33 +1,52 @@
 // 👉 1- Finding an element on the page and saving a reference to it
 //  Older: getElementById, getElementsByTagName, getElementsByClassName
 //  Newer: querySelector, querySelectorAll
-//  Select the following single elements from the div.card
 
+const mainNav = document.getElementById("mainNav")
+const divElems = document.getElementsByClassName("div")
+const cardElems = document.getElementsByClassName("card")
+
+const queryMainNav = document.querySelector("#mainNav")
+const queryDivElems = document.querySelectorAll("div")
+const queryCardElems = document.querySelectorAll(".card")
+
+//  Select the following single elements from the div.card
 // A- finding across the entire DOM
-const header = null
-const logoTitle = null
-const firstCard = null
+const header = document.querySelector("header")
+const logoTitle = document.querySelector("#logoTitle")
+const firstCard = document.querySelector(".card")
+
 // B- finding within one particular element
-const imageFirstCard = null
-const titleFirstCard = null
-const subtitleFirstCard = null
-const textFirstCard = null
+const imageFirstCard = firstCard.querySelector("image")
+const titleFirstCard = firstCard.querySelector("h2")
+const subtitleFirstCard = firstCard.querySelector("h3")
+const textFirstCard = firstCard.querySelector("p")
 // C- traversing with dot notation
-const link1FirstCard = null
-const link2FirstCard = null
+const link1FirstCard = textFirstCard.nextElementSibling
+const link2FirstCard = link1FirstCard.nextElementSibling
 
 
 // 👉 2- Finding collections of elements in the DOM
 // A- Find all the anchor tags inside the nav element
-// B- Loop over the links and console.log their text content
-// C- Turn the collection of links into a real array
-// D- Use .filter to find the anchor tag with the textContent of "Home"
+const links = document.querySelectorAll("nav a")
 
+// B- Loop over the links and console.log their text content
+links.forEach(link => console.log(link.textContent))
+
+// C- Turn the collection of links into a real array
+const linksArr = Arry.from(links)
+
+// D- Use .filter to find the anchor tag with the textContent of "Home"
+const homeLink = linksArr.find(link => link.textContent === "Home")
 
 // 👉 3- Changing an element's text content
 //  A- Change the cat-related content into dog-related content
 //  B- Have the students research online the difference between textContent and innerText
-
+logoTitle.textContent = "Bloomtech Doggos"
+titleFirstCard.textContent = "I is DOGGGG!"
+subtitleFirstCard.textContent = "A story in three parts"
+textFirstCard.textContent = "Dogs are nice, dogs are kind, stay away from cats"
+link2FirstCard.textContent = "Dog Ipsum"
 
 // 👉 4- Changing any property
 //  A- Using dot notation to change a few attributes
